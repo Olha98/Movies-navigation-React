1b509608a22e8d51ef5f57e2aade0fb91c6b164d
Olha Hrymchak <forwork1177@gmail.com>
2020-08-26 14:04:46 +0300

    added

--- src/Views/MovieDetailesPage/MovieDetailesPage.js
+++ src/Views/MovieDetailesPage/MovieDetailesPage.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom';
+import React, { Component, lazy, Suspense } from 'react'
 import MovieDetailesServices from '../../Services/MovieDetailesServices'
-import MovieDetailesList from './MovieDetailesList/MovieDetailesList';
-import MovieDetailesMore from './MovieDetailesMore/MovieDetailesMore';
+import { withRouter } from 'react-router-dom';
+import Spinner from '../../Components/Loader/Loader'
+import style from '../../Components/Loader/Loader.module.css'
 import css from './MovieDetailesPage.module.css'
 
+const MovieDetailesList = lazy(() => import('./MovieDetailesList/MovieDetailesList'));
+const MovieDetailesMore = lazy(() => import('./MovieDetailesMore/MovieDetailesMore'));
 
 
 class MovieDetailesPage extends Component {
@@ -26,6 +28,10 @@ class MovieDetailesPage extends Component {
       });
 
    
+  }
+  
+  getLoading = () => {
+    return <div className={style.loading}><Spinner/></div>
   }
 
 
@@ -50,8 +56,10 @@ class MovieDetailesPage extends Component {
     
     return (
       <section className={css.section}>
+        <Suspense fallback={this.getLoading()}>
         <MovieDetailesList infoFilm={infoFilm} goBack={this.goBack} props={this.props} />
         <MovieDetailesMore infoFilm={infoFilm} />
+        </Suspense>
       </section>
 
     )
--- src/Views/NotFound/NotFound.js
+++ src/Views/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import gif404 from '../../image/404 (1).gif'
+import css from './NotFound.module.css'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <>
-    <p>NotFound</p>
-    </>
+    <section className={css.section_NotFound}>
+    <img src={gif404} alt="/" width="500"/>
+    <h2>Look like you're lost...</h2>
+    <p>The page you are looking for not avaible!</p>
+    <button><Link to='/' className={css.Link_NotFound}>Go to Home</Link></button>
+    </section>
   )
 }
